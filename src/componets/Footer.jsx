89dcdd2ddb362f0499e7c0d20f80ca199a868415
clasesucatmarlon <clasesucatmarlon@gmail.com>
2021-04-15 17:02:44 -0500

    Agregando componente Footer

---
+++ src/componets/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../index.css'
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            <p className="footer-title">Medellín &copy; 2021 - Marlon García </p> 
+        </div>
+    )
+}
+
+export default Footer;
