64207650e291c900f4d65708f744dd931021e445
clasesucatmarlon <clasesucatmarlon@gmail.com>
2021-04-15 17:03:10 -0500

    Agregando componente Header

---
+++ src/componets/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../index.css'
+
+const Header = () => {
+    return (
+        <div className="header">
+            <p className="header-title">Administrador de productos</p> 
+        </div>
+    )
+}
+
+export default Header;
